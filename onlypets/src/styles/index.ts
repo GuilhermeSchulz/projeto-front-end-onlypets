b2dac9fe45d6cb5a44f602721ce4b3ddec884e3a
Guilherme Schulz <gui.schulz@gmail.com>
2022-11-01 20:55:12 -0300

    style: created global reset style

---
+++ onlypets/src/styles/index.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    font-family: 'Montserrat', sans-serif;
+    list-style:none;
+    box-sizing: border-box;
+}
+`;
