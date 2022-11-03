f54af9d14885536088038ac09f1cfccb671dce80
jessicacolombo <jessicacolombo384@gmail.com>
2022-11-03 11:00:27 -0300

    fix: remove input component

--- onlypets/src/components/Inputs/styles.ts
+++ onlypets/src/components/Inputs/styles.ts
@@ -1,32 +1,29 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Label = styled.label`
-    display: flex;
-    gap: 10px;
-    flex-direction: column;
-    font-size: var(--text-size-2);
-    color: var(--black);
-`
+export const StyledFieldInput = styled.label`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  font-size: var(--text-size-2);
+  color: var(--black);
 
-export const Input = styled.input`
+  input,
+  select {
     height: 38px;
     width: 300px;
     border: var(--color-primary-1) 1px solid;
     color: var(--color-primary-hover);
     padding-left: 5px;
-    
     border-radius: 4px;
 
     &::placeholder {
-        color: var(--color-primary-1);
+      color: var(--color-primary-1);
     }
 
-    &:focus{
-        box-shadow: 0 0 0 0;
-        outline: 0;
-        border-color: var(--color-primary-hover);
-        
+    &:focus {
+      box-shadow: 0 0 0 0;
+      outline: 0;
+      border-color: var(--color-primary-hover);
     }
-`
-
-
+  }
+`;
