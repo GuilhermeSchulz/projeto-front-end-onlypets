be5bef79cbf8df7ed50f14c9ce163a8659da2f9e
wilkeran <wilkeran.costs@gmail.com>
2022-11-03 11:45:51 -0300

    feat: created Register component

--- onlypets/src/App.tsx
+++ onlypets/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Register } from './components/Register';
 import { GlobalStyles } from './styles';
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <Register/>
     </>
   );
 }
