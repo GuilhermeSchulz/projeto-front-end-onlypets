e18da35a37f516e2f6462515e1e2f5ec1b341083
Marcela <mamadipaula@gmail.com>
2022-11-02 21:51:57 -0300

    fix: input Component

--- onlypets/src/components/Inputs/styles.ts
+++ onlypets/src/components/Inputs/styles.ts
@@ -29,25 +29,4 @@ export const Input = styled.input`
     }
 `
 
-// export const Select = styled.select`
-//     height: 38px;
-//     width: 300px;
-//     border: var(--color-primary-1) 1px solid;
-//     color: var(--color-primary-hover);
-//     padding-left: 5px;
-    
-//     border-radius: 4px;
-
-//     &::placeholder {
-//         color: var(--color-primary-1);
-//     }
-
-//     &:focus{
-//         box-shadow: 0 0 0 0;
-//         outline: 0;
-//         border-color: var(--color-primary-hover);
-        
-//     }
-
-// `
 
